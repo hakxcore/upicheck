0859d17d9685dc8966cca067fba5bf22d9b151fa
Mukesh Kumar <kumarm_1@rknec.edu>
2023-04-26 00:28:57 +0530

    Update upi-check.py

--- upi-check.py
+++ upi-check.py
@@ -1,17 +1,20 @@
 import requests
-target=input("\n[+] Enter the target Phone Number or UPI ID:")
-ids=["apl", "ybl", "oksbi", "okhdfcbank", "axl", "paytm","ibl", "upi", "icici", "sbi", "kotak", "postbank", "axisbank", "okicici", "okaxis", "dbs", "barodampay", "idfcbank"]
-url="https://upibankvalidator.com/api/upiValidation?upi="
+
+target = input("\n[+] Enter the target Phone Number or UPI ID:")
+ids = ["apl", "ybl", "oksbi", "okhdfcbank", "axl", "paytm", "ibl", "upi", "icici", "sbi", "kotak", "postbank", "axisbank", "okicici", "okaxis", "dbs", "barodampay", "idfcbank"]
+url = "https://upibankvalidator.com/api/upiValidation?upi="
+
 print("")
-if target.find('@')!=-1:
-    response=requests.post(url+target).json()
-    if response['isUpiRegistered']: print("[+]UPI ID registered with name " + response['name'])
+if target.find('@') != -1:
+    response = requests.post(url+target).json()
+    if response['isUpiRegistered']:
+        print("[+]UPI ID registered with name " + response['name'])
     else:
         print("[-] UPI ID not registered!")
 else:
     for i in ids:
-        response=requests.post(url+target+'@'+i).json()
+        response = requests.post(url+target+'@'+i).json()
         if response["isUpiRegistered"]:
-            print("[+] UPI ID registered with name " + response['name']+" with" +i)
+            print("[+] UPI ID registered with name " + response['name']+" with " + i)
         else:
-            print("[-]UPI ID not registered with " +i)
+            print("[-] UPI ID not registered with " + i)
